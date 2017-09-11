9efbe55ebb823fafc4f5c4a84f55ee75bacf2239
chak808 <30875250+chak808@users.noreply.github.com>
2017-09-11 08:21:48 -1000

    Create DeathNote.js

---
+++ DeathNote.js
@@ -0,0 +1,4 @@
+function deathNote() {
+	var death =Math.floor(Math.random() * 4);
+  document.getElementsByClassName('stem')[death].innerHTML="<img src='https://cdnb.artstation.com/p/assets/images/images/000/227/915/large/ivan-mityaev-skull-front.jpg?1443931826' width='10%'>"
+}
