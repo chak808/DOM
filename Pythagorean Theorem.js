229a57b6e33c6f7de825e11a00dcac2713702808
chak808 <30875250+chak808@users.noreply.github.com>
2017-09-13 09:08:02 -1000

    Create Pythagorean Theorem.js

---
+++ Pythagorean Theorem.js
@@ -0,0 +1,5 @@
+function showC() {
+var a = document.getElementById('a').value;
+var b = document.getElementById('b').value;
+document.getElementById('c').innerHTML= Math.sqrt(Math.pow(a,2) + Math.pow(b,2));
+}
